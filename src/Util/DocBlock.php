721f718b0e7811ceb91ab84062df687f5b747bcf
Yurun <admin@yurunsoft.com>
2024-04-18 19:00:28 +0800

    修复测试

--- src/Util/DocBlock.php
+++ src/Util/DocBlock.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Imi\Util;
 
-use phpDocumentor\Reflection\DocBlock as RealDocBlock;
 use phpDocumentor\Reflection\DocBlockFactory;
+use phpDocumentor\Reflection\DocBlockFactoryInterface;
 use phpDocumentor\Reflection\Location;
 use phpDocumentor\Reflection\Types\Context;
 
@@ -13,9 +13,9 @@ class DocBlock
 {
     use \Imi\Util\Traits\TStaticClass;
 
-    private static ?DocBlockFactory $factory = null;
+    private static ?DocBlockFactoryInterface $factory = null;
 
-    public static function getFactory(): DocBlockFactory
+    public static function getFactory(): DocBlockFactoryInterface
     {
         if (null === self::$factory)
         {
@@ -28,7 +28,7 @@ class DocBlock
     /**
      * @param object|string $docblock a string containing the DocBlock to parse or an object supporting the getDocComment method (such as a ReflectionClass object)
      */
-    public static function getDocBlock($docblock, ?Context $context = null, ?Location $location = null): RealDocBlock
+    public static function getDocBlock($docblock, ?Context $context = null, ?Location $location = null): DocBlockFactoryInterface
     {
         return self::getFactory()->create($docblock, $context, $location);
     }

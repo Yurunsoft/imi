f358cfd9ed153ad526ddba7bfea3588eec4885c2
Yurun <admin@yurunsoft.com>
2024-05-02 08:58:25 +0800

    修复测试 (#696)
    * 修复 Swoole 测试的 Event::wait() 警告报错 (#693)
    
    * 修复 Swoole 测试的 Event::wait() 警告报错
    
    * 修复测试
    
    * 修复测试
    
    * 修复测试
    
    * 修复测试
    
    * 修复测试

--- src/Components/grpc/src/Middleware/ActionMiddleware.php
+++ src/Components/grpc/src/Middleware/ActionMiddleware.php
@@ -218,6 +218,10 @@ class ActionMiddleware implements MiddlewareInterface
                 $headers[$name] = implode(', ', $values);
             }
         }
+        else
+        {
+            $allData = [];
+        }
 
         /** @var ActionMethodItem[] $actionMethodCache */
         foreach ($actionMethodCache as $actionMethodCacheItem)
